80e82a80b33863a932d7e52930409ff9faa3b11d
Joch <jochnet@gmail.com>
2026-01-22 08:53:27 -0300

    chore: finalize configs (eslint, prettier, jest, netlify, tsconfig)

---
+++ jest.config.cjs
@@ -0,0 +1,6 @@
+module.exports = {
+  testEnvironment: "node",
+  testMatch: ["**/__tests__/**/*.test.(js|ts)", "**/?(*.)+(spec|test).(js|ts)"],
+  collectCoverage: true,
+  coverageDirectory: "coverage"
+};
